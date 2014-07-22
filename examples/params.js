cf9a70d9ad62811c9e7b8f7e608a5337166fec6f
theptrk <patrick.tran06@gmail.com>
2014-07-21 18:00:35 -0700

    strip down to params

--- examples/params.js
+++ examples/params.js
@@ -1,13 +1,5 @@
 var Hapi = require('hapi');
 var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
-  
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello Hapi');
-    }
-});
 
 server.route({
   path: '/{name}/{veg}', 
