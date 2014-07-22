433b4cb395ff4339774f450cc26dd699bc9b6325
theptrk <patrick.tran06@gmail.com>
2014-07-21 18:09:02 -0700

    Serve Static

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var Hapi = require('hapi');
 var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
   
+// hello hapi
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
         reply('Hello Hapi');
     }
+});
+
+// params
+server.route({
+  path: '/{name}/{veg}', 
+  method:'GET', 
+  handler:function(request, reply){
+    reply('Hello ' + request.params.name + 
+      ', have some ' + request.params.veg + '!');
+  }
+});
+
+// file serving
+server.route({
+  path: '/serveindex', 
+  method:'GET', 
+  handler: {
+    file: "index.html"
+  }
 });
 
 server.start();
